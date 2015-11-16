6329fb90eef1db549ce7bd60e104c3b66511871e
Chris <cmking@colgate.edu>
2015-11-16 16:05:28 -0500

    Look at clone

--- proc.c
+++ proc.c
@@ -175,25 +175,23 @@ fork(void)
 
 int clone(void(*fcn)(void*), void *arg, void *stack) { // FIX THIS !
   int pid, i;
-  struct proc *newtask;
-  // Copy process state from p.
-  /*if((np->pgdir = copyuvm(proc->pgdir, proc->sz)) == 0){
-    kfree(np->kstack);
-    np->kstack = 0;
-    np->state = UNUSED;
-    return -1;
-  }*/
+  struct proc *newtask = 
+  if ((newtask = allocproc()) == 0) return -1;
   
-  // Allocate process.
-  if((newtask = allocproc()) == 0)
-    return -1;
+  
+  
+  
+  
+  
+  newtask->kstack = stack;
   newtask->isThread = 1; // labelling as thread
-  newtask->sz = PGSIZE;
+  newtask->sz = proc->sz;
   newtask->parent = proc; // perm
   *newtask->tf = *proc->tf;
+  newtask->
 
   // Clear %eax so that clone returns 0 in the child.
-  newtask->tf->eax = 0;
+  newtask->tf->eax = newtask->pid;
 
   for(i = 0; i < NOFILE; i++)
     if(proc->ofile[i])
@@ -245,7 +243,7 @@ int join(int pid) { // If question, seek CK
 	return wr;
 	*/
 	struct proc *p; // started work on join, but clone still doesn't work
-  	int havekids, pid;
+  	int havekids;
 
   	acquire(&ptable.lock);
   	for(;;){
